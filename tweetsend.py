92e3ca00a4f379c5b33f3eebf22a189a130dbb2f
sagarp31 <sagarp3199@gmail.com>
2020-07-03 17:25:12 +0530

    Deleting Extra FIles

--- tweetsend.py
+++
@@ -1,7 +0,0 @@
-from twython import Twython
-C_K = "my key"
-C_SK = "my secret key"
-A_K = "Token key"
-A_SK = "Secret Token key"
-api = Twython(C_K,C_SK,A_K,A_SK)
-api.update_status(status="Any message")
